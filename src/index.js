b504f6134f5aa27bac8cef92b170fb672b715710
Sergio Sisternes <sesispla@outlook.com>
2017-08-24 15:20:11 +0200

    Installed chalk and added new start scripts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 /*jslint node: true */
 "use strict";
 
+var config = require('./config.js');
+var render = require('./render.js');
+var program = require("commander");
+
+var commands = {
+    cfg: new config(),
+    build: function() {    
+        var cfg = new config();
+        var r = new render(this.cfg);
+        r.render();        
+    },
+    serve: function() {
+        var server = new liveserver(this.cfg);
+    }
+}
+
+program.version("0.1.0")
+    .option("build", "Build the documentation")
+    .option("serve", "Start the server")
+    .parse(process.argv);
+
 var liveserver = require('./liveserver.js');
 var config = require('./config.js');
 
-var cfg = new config();
-var server = new liveserver(cfg);
+if (program.build) {
+    commands.build();
+}
+else if (program.serve) {
+    commands.serve();
+}
