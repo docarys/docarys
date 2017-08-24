8d218f4cd59b93400cafac6c9603655fa42f7ce8
Sergio Sisternes <sesispla@outlook.com>
2017-08-24 19:58:41 +0200

    Fixed an issue preventing mydocs to work out of the development path

--- src/config.js
+++ src/config.js
@@ -7,18 +7,22 @@ var path = require('path');
 function Config(filename) {
     /** Filename to use */
     this.filename = filename ? filename : 'mydocs.yml';
+    /** Running path */
+    this.runPath = process.cwd();
+    /** Module path */
+    this.modulePath = __dirname;
     /** Full path to the file, starting from current dir */
-    this.fullpath = path.resolve(__dirname + '/' + this.filename);
+    this.fullpath = path.resolve(this.runPath + '/' + this.filename);
     /** Variable context, resulting from the load of the YAML file */
     this.context = yamljs.load(this.fullpath);
     /** Full path where the source documents are stored */
-    this.sourcePath = path.resolve(__dirname + '/' + (this.context['sourceDir'] ? this.context['sourceDir'] : 'docs'));
+    this.sourcePath = path.resolve(this.runPath+ '/' + (this.context['sourceDir'] ? this.context['sourceDir'] : 'docs'));
     /** Full path where output should be stored */
-    this.targetPath = path.resolve(__dirname + '/' + (this.context['targetDir'] ? this.context['targetDir'] : 'build'));
+    this.targetPath = path.resolve(this.runPath+ '/' + (this.context['targetDir'] ? this.context['targetDir'] : 'build'));
     /** Path where the template is stored */
-    this.templateDir = path.resolve(__dirname + '/' + (this.context['theme_dir'] ? this.context['theme_dir'] : 'default'));   
+    this.templateDir = path.resolve(this.modulePath + '/' + (this.context['theme_dir'] ? this.context['theme_dir'] : 'default'));   
     /** Template full path */
-    this.templatePath = path.resolve(__dirname + '/' + this.context['theme_dir']);
+    this.templatePath = path.resolve(this.modulePath + '/' + this.context['theme_dir']);
 
     return {
         /** Configuration context */
